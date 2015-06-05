3a5bb9ee74ca6c9bd36e57bf043ebff37bda8564
mickey_hao <moshao_0920@163.com>
2015-06-05 14:51:59 +0800

    campaign

---
+++ app/Http/routes.php
@@ -0,0 +1,10 @@
+<?php
+Route::get('/campaign/{campaign_id}', 'CampaignController@campaign')->where('campaign_id', '[0-9]+');
+Route::get('/client/{client_id}', 'CampaignController@client')->where('client_id', '[0-9]+');
+
+Route::get('/{campaign_id}', 'CampaignController@campaign')->where('campaign_id', '[0-9]+');
+Route::get('/{campaign_id}/keywords', 'CampaignController@keywords')->where('campaign_id', '[0-9]+');
+Route::get('/{campaign_id}/event', 'CampaignController@event')->where('campaign_id', '[0-9]+');
+Route::get('/{campaign_id}/channel', 'CampaignController@channel')->where('campaign_id', '[0-9]+');
+
+Route::get('/location', 'CampaignController@location');
---
+++ app/Model/Campaign.php
@@ -0,0 +1,10 @@
+<?php namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Campaign extends Model
+{
+
+    protected $table = 'campaign';
+//    protected $visible = ['id', 'client_id', 'name'];
+}
