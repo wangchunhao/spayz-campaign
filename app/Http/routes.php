7ffdf31e77afe7af86578b4e8bbf35bcda06ae77
mickey_hao <moshao_0920@163.com>
2015-06-05 15:28:37 +0800

    campaign

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 Route::get('/campaign/{campaign_id}', 'CampaignController@campaign')->where('campaign_id', '[0-9]+');
 Route::get('/client/{client_id}', 'CampaignController@client')->where('client_id', '[0-9]+');
 
@@ -9,12 +8,3 @@ Route::get('/{campaign_id}/event', 'CampaignController@event')->where('campaign_
 Route::get('/{campaign_id}/channel', 'CampaignController@channel')->where('campaign_id', '[0-9]+');
 
 Route::get('/location', 'CampaignController@location');
-=======
-
-Route::get('/campaign/{campaign_id}', 'CampaignController@campaign')->where('campaign_id', '[0-9]+');
-Route::get('/client/{client_id}', 'CampaignController@client')->where('client_id', '[0-9]+');
-
-
-Route::get('/{campaign_id}', 'CampaignController@campaign')->where('campaign_id', '[0-9]+');
-Route::get('/{campaign_id}/keywords', 'CampaignController@keywords')->where('campaign_id', '[0-9]+');
->>>>>>> fc735e5ce7655de35d9a0bc73b69bc7cbd685680
