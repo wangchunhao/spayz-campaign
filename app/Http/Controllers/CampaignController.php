1a5f696a107ac9a299c5c23c6cb6aaf7814aa620
mickey_hao <moshao_0920@163.com>
2015-06-11 17:52:46 +0800

    add filter save

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -6,8 +6,10 @@ use App\Model\KeywordGroups;
 use App\Model\Event;
 use App\Model\Channel;
 use App\Model\Location;
+use App\Model\FilterSave;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Input;
+use Illuminate\Http\Request;
 
 
 class CampaignController extends Controller {
@@ -30,6 +32,7 @@ class CampaignController extends Controller {
         return response()->json($data);
 
 	}
+	
     public function client($client_id)
     {
         $data = Client::find($client_id);
@@ -40,6 +43,11 @@ class CampaignController extends Controller {
         	return response()->json(['code'=>'404','message'=>'No message']);
         }
     }
+    
+    /*
+     * Keywords
+     * 关键词信息获取
+     */
     public function keywords($campaign_id){
     	
     	$data = KeywordGroups::select(DB::raw('keyword.id,campaign_id,keyword_group.name,keyword_group.description,keyword.name as keyword,keyword.name_en,keyword.enabled'))
@@ -54,6 +62,11 @@ class CampaignController extends Controller {
     	}
     	
     }
+    
+    /*
+     * Event
+   	 * campaign活动信息
+   	 */
     public function event($campaign_id){
     	$page = (Input::get('page'))?Input::get('page'):0;
     	$data = Event::where('campaign_id','=',$campaign_id)
@@ -91,6 +104,78 @@ class CampaignController extends Controller {
     	}else{
     		return response()->json(['code'=>'404','message'=>'No message']);
     	}
+    }
+    
+    /*
+     * Save search
+    * 塞选条件保存
+    *
+    * @pragram int id 条件id 检查是否存在
+    * @pragram int campaign_id
+    * @pragram int admin_id 当前登陆用户ID
+    * @pragram string type 条件类型
+    * @pragram string name 条件名称
+    * @pragram string filters 要保存的条件    (可以在URL添加：&gender=m&age=25)
+    *
+    */
+    
+    public function filter_save(Request $request){
+    
+    	$receive = Input::all();
+    	
+    	$admin_id = $receive['admin_id'];
+    	$campaign_id = $receive['campaign_id'];
+    	$name = $receive['type'];
+    	$type = $receive['name'];
+    	
+    	unset($receive['admin_id']);
+    	unset($receive['campaign_id']);
+    	unset($receive['type']);
+    	unset($receive['name']);
+    	
+    	$filter = '';
+    	foreach ($receive as $k=>$v){
+    		if($k != 'id'){
+    			$filter .= '&'.$k.'='.$v;
+    		}
+    	}
+    	if(isset($receive['id'])){
+    		FilterSave::firstOrCreate(['id' => $receive['id']]);
+    		$data = FilterSave::find($receive['id']);
+    	}else{
+    		$data = new FilterSave;
+    	}
+    
+    	$data->admin_id = $admin_id;
+    	$data->campaign_id = $campaign_id;
+    	$data->type = $type;
+    	$data->name = $name;
+    	$data->created_at = date('Y-m-d H:i:s');
+    	$data['filter'] = ltrim($filter,'&');
+    		
+    	$is = $data->save();
+    	
+    	if($is){
+    		return Response()->json(['code' => '200', 'message' => 'Access'])->setCallback($request->input('callback'));
+    	}else{
+    		return Response()->json(['code' => '400', 'message' => 'Failed'])->setCallback($request->input('callback'));
+    	}
+    
+    }
+    
+    /*
+     * Get filter
+     * 获取全部保存的塞选条件
+     */
+    public function get_filter(Request $request){
+    	
+    	$data = FilterSave::get();
+    	
+    	if($data){
+			return Response()->json(['collection' => $data])->setCallback($request->input('callback'));
+		}else{
+			return Response()->json(['code' => '404', 'message' => 'No Message'])->setCallback($request->input('callback'));
+		}
     }
 
 }
