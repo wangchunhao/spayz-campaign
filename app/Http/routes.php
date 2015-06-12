3b844977d871f8711bbdb655254b9a9f3805c39a
mickey_hao <moshao_0920@163.com>
2015-06-12 14:41:49 +0800

    routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,7 +4,6 @@ Route::get('/client/{client_id}', 'CampaignController@client')->where('client_id
 Route::get('/filter', 'CampaignController@filter_save');
 Route::get('/getFilter', 'CampaignController@get_filter');
 
-
 Route::get('/{campaign_id}', 'CampaignController@campaign')->where('campaign_id', '[0-9]+');
 Route::get('/{campaign_id}/keywords', 'CampaignController@keywords')->where('campaign_id', '[0-9]+');
 Route::get('/{campaign_id}/event', 'CampaignController@event')->where('campaign_id', '[0-9]+');
