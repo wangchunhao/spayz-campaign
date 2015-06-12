b7e56e0f92e11e375f6fb3c15af0a99fbeb5f8af
mickey_hao <moshao_0920@163.com>
2015-06-12 09:29:04 +0800

    Revert "Revert "add filter save""
    This reverts commit a6f9480b12f9f9267eb63634feca1cdfcdc3ab9f.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,6 +1,9 @@
 <?php
 Route::get('/campaign/{campaign_id}', 'CampaignController@campaign')->where('campaign_id', '[0-9]+');
 Route::get('/client/{client_id}', 'CampaignController@client')->where('client_id', '[0-9]+');
+Route::get('/filter', 'CampaignController@filter_save');
+Route::get('/getFilter', 'CampaignController@get_filter');
+
 
 Route::get('/{campaign_id}', 'CampaignController@campaign')->where('campaign_id', '[0-9]+');
 Route::get('/{campaign_id}/keywords', 'CampaignController@keywords')->where('campaign_id', '[0-9]+');
